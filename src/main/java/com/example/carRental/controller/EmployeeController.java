97c3cc78bee7ad9d320f8823287c0a88bad60894
morkmic <126565671+morkmic@users.noreply.github.com>
2023-04-04 20:12:42 +0200

    Employee controller

---
+++ src/main/java/com/example/carRental/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.example.carRental.controller;
+
+import com.example.carRental.model.Employee;
+import com.example.carRental.model.User;
+import com.example.carRental.repository.EmployeeRepository;
+import com.example.carRental.repository.UserRepository;
+import com.example.carRental.service.EmployeeService;
+import com.example.carRental.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Configuration
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity getEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return ResponseEntity.ok(employees);
+    }
+
+    @GetMapping("/{employeeId}")
+    public ResponseEntity getEmployee(@PathVariable Long employeeId) {
+        Employee employee = employeeService.getEmployee(employeeId);
+        return ResponseEntity.ok(employee);
+
+    }
+
+  //  @PostMapping("/addUser")
+/*    public ResponseEntity addUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.addUser(user));
+    }*/
+
+    @DeleteMapping("/{employeeId}")
+    public ResponseEntity deleteEmployee(@PathVariable Long employeeId) {
+         employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
